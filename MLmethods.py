4c4134fc72d18c7589de3487108e2e2ab57b7aa1
RuoxueWang-lab <ruoxue.wang@outlook.com>
2024-12-19 23:34:22 +0000

    Implemented lasso regression functions.

--- MLmethods.py
+++ MLmethods.py
@@ -1,5 +1,5 @@
 import numpy as np
-import GredienrDecent
+import GradientDescent
 
 def LinearRegression(X, y):
     """
@@ -19,7 +19,7 @@ def LinearRegression(X, y):
     return w
 
 
-def RidgeRegression(X, y, l2, exclude_x0=False):
+def RidgeRegression(X, y, l2=0, exclude_x0=False):
     """
     Implement L2-penalised least-squares (ridge) regression
     using its closed form expression.
@@ -81,7 +81,7 @@ def logistic_regression_with_batch_GD (X, y, w0=None, lr=0.05,
     def logistic_forward ( X, w ):
         return sigmoid(X @ w)
 
-    def logistic_loss ( X, y, w, eps=1e-10 ):
+    def logistic_loss ( X, y, w, eps=1e-10 ): #eps is used to prevent log becoming undefined
         g = logistic_forward(X, w)
         return (np.dot(-y, np.log(g + eps)) - np.dot((1 - y), np.log(1 - g + eps)))/len(y)
 
@@ -92,7 +92,7 @@ def logistic_regression_with_batch_GD (X, y, w0=None, lr=0.05,
     if w0 is None: 
         w0 = np.zeros(X.shape[-1])
 
-    return GredienrDecent.batch_gradient_descent( w0,
+    return GradientDescent.batch_gradient_descent( w0,
                                                   loss_func = lambda z: logistic_loss(X, y, z),
                                                   grad_loss_func = lambda z: logistic_grad(X, y, z),
                                                   lr = lr,
@@ -147,7 +147,7 @@ def logistic_regression_with_stochastic_GD ( X, y, batch_size=1, w0=None, lr=0.0
     if w0 is None:
         w0 = np.zeros((X.shape[-1]))   
     
-    return GredienrDecent.stochastic_gradient_descent(w0,
+    return GradientDescent.stochastic_gradient_descent(w0,
                                                       loss_func = lambda w: logistic_loss(X, y, w),
                                                       grad_loss_func = logistic_grad,
                                                       data = data,
@@ -156,6 +156,56 @@ def logistic_regression_with_stochastic_GD ( X, y, batch_size=1, w0=None, lr=0.0
                                                       loss_stop=loss_stop, w_stop=weight_stop, max_iter=max_iter)
     
 
+def LassRegression_with_BatchGD(X, y, l1=0, excluede_x0=False, w0=None, lr=0.05,
+                                loss_stop=1e-4, weight_stop=1e-4, max_iter=100):
+    """
+    Implement L1-penalised least-squares (lasso) regression
+    using its closed form expression.
+
+    # Arguments
+        X: an array of sample data, where rows are samples
+           and columns are features (assume there are at least
+           as many samples as features). caller is responsible
+           for prepending x0=1 terms if required.
+        y: vector of measured (or simulated) labels for the samples,
+           must be same length as number of rows in X
+        l1: optional L1 regularisation weight. if zero (the default)
+           then this reduces to unregularised least squares
+        exclude_x0 (bool): whether to treat the first data feature as representing
+           an intercept term and exclude it from the regularisation.
+           note that this defaults to False, meaning regularise everything
+        w0: starting value of the weights, if omitted
+           then all zeros are used
+        lr: learning rate, ie fraction of gradients by
+           which to update weights at each iteration
+        loss_stop: stop iterating if the loss changes
+            by less than this (absolute)
+        weight_stop: stop iterating if weights change by less
+            than this (L2 norm)
+        max_iter: stop iterating after iterating this
+            many times
+         
+    # Returns
+        ws: a list of fitted weights at each iteration
+        losses: a list of the loss values at each iteration
+    """
+    def lasso_loss(X, y, w, l1, exclude_x0): 
+        reg = np.identity(X.shape[1])
+        if exclude_x0:
+        # Since if we don't want to regulize the intercept, we set i_{1,1} of the indentity matrix to be 0
+            reg[0,0] = 0
+        return (np.norm(X @ w - y))**2 - l1 * reg * np.norm(w,1)
+            
+    
+    def lasso_grad(l1, w):
+        l1_subgradient = np.where(w > 0, 1, np.where(w < 0, -1, 0))
+        return l1 * l1_subgradient
+    
+    return GradientDescent.batch_gradient_descent(w0,
+                                                  loss_func=lambda w: lasso_loss(X,y,w,l1,excluede_x0),
+                                                  grad_loss_func=lambda w: lasso_grad(l1,w),
+                                                  lr = lr,
+                                                  loss_stop=loss_stop, w_stop=weight_stop, max_iter=max_iter)
     
     
     
